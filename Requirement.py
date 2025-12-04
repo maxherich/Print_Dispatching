750f40e28cd51388899cc6511e6a438ad3e9a7f2
maxherich0 <herich@pixelfield.cz>
2025-12-04 22:37:48 +0100

    Print Dispatching 1.0

--- Requirement.py
+++ Requirement.py
@@ -8,5 +8,5 @@ class Requirement:
     def execute(self): # This method will be connected to the printers and tell to print
         print(f"Requirement {self.number} is being printed")
         time.sleep(3)
-        with open(f"./Printed/{self.path}/print{self.number}.txt", "w") as file:
+        with open(f"./Printed/print{self.number}.txt", "w") as file:
             file.write(f"Print number {self.number}")
